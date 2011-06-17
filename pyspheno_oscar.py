eba39edbd7e42271a76de299819838d890d1a389
Oscar Zapata <oscarovich@gmail.com>
2011-06-17 17:25:39 -0500

    pyspheno_oscar.py only in python

--- pyspheno_oscar.py
+++ pyspheno_oscar.py
@@ -1,61 +1,70 @@
 #!/usr/bin/env python
 
 import numpy as np
-#call spheno with fit on to obtain mu and vd
-
 import commands
-#lsout=commands.getoutput('ls')
+import pyslha
+## Run Spheno with lesHouches.in
 commands.getoutput('cp LesHouches.in.neutrinofits-ON LesHouches.in')
 commands.getoutput('./bin/SPheno')
-import pyslha
-spc=pyslha.readSLHAFile('SPheno.spc')
-eps=spc[0]['RVKAPPA']
-eps1=eps.entries[1]
-eps2=eps.entries[2]
-eps3=eps.entries[3]
-veps=np.array( [eps1,eps2,eps3] )
-snvev=spc[0]['RVSNVEV']
-snvev1=snvev.entries[1]
-snvev2=snvev.entries[2]
-snvev3=snvev.entries[3]
-vsnvev=np.array( [snvev1,snvev2,snvev3] )
-sphenorp=spc[0]['SPHENORP']
-lamb1=sphenorp.entries[4]
-lamb2=sphenorp.entries[5]
-lamb3=sphenorp.entries[6]
-vd=sphenorp.entries[15]
-vu=sphenorp.entries[16]
-vlamb=np.array( [lamb1,lamb2,lamb3] )
-vmu=(vlamb-vd*veps)/vsnvev
-mu=vmu[1]
-#To write rp parameters
+##Determine mu
+spc,decays=pyslha.readSLHAFile('SPheno.spc')
+veps=np.asarray(spc['RVKAPPA'].entries.values())
+vsnvev=np.asarray(spc['RVSNVEV'].entries.values())
+vlam=np.asarray(spc['SPHENORP'].entries.values()[0:3])
+vd=spc['SPHENORP'].entries[15]
+mu=((vlam-vd*veps)/vsnvev)[0]
+##To write rp parameters and neutrino 
 datos= open('solutions.out','w')
-#begin for cycle
-for i in range(1,10001):
+#begin the iterations 
+for i in range(1,11):
     if i%100==0:
         print "i=",i
 #random generation of epsilon_i and lambda_i
-    sgn=(-1)**np.random.random_integers(1,2,3)
-    epsi=np.random.uniform(-1,1,3)
-    lambi=np.random.uniform(-1,1,3)
+#    sgn=(-1)**np.random.random_integers(1,2,3)
+    sgn=np.array([1.0,1.0,-1.0])
+#    epsi=np.random.uniform(-1,1,3)
+#    epsi=np.array([np.random.uniform(3e-2,0.6),np.random.uniform(1e-5,1),np.random.uniform(1e-5,1)])*(-1)**np.random.random_integers(1,2,3)
+    A1=np.log10([8e-2,8e-2,8e-2])    
+    B1=np.log10([1e-1,1e-1,1e-1])    
+    epsi=10**((B1-A1)*np.random.uniform(0,1,3)+A1)*sgn    
+#    lambi=np.random.uniform(-1,1,3)
+#    lambi=np.array([np.random.uniform(1e-5,6e-2),np.random.uniform(3e-2,7e-1),np.random.uniform(3e-2,7e-1)])*(-1)**np.random.random_integers(1,2,3)
+    A2=np.log10([2e-3,5e-2,5e-2])    
+    B2=np.log10([4e-3,7e-2,7e-2])    
+    lambi=10**((B2-A2)*np.random.uniform(0,1,3)+A2)    
     vi=(lambi-epsi*vd)/mu
-    salida = open('salida.out', 'w')
-    print >> salida, lambi[0], lambi[1], lambi[2], epsi[0], epsi[1], epsi[2]
-##to check with original SPheno.spc
-#print >> salida, lamb1, lamb2, lamb3, eps1, eps1, eps2 
-    salida.close()
-    commands.getoutput('./main.sh')
+##  Reading LesHouches_MASS.in 
+    LesHouches,decays=pyslha.readSLHAFile('LesHouches_MASS.in')
+    LesHouches['RVKAPPAIN'].entries[1]=epsi[0]
+    LesHouches['RVKAPPAIN'].entries[2]=epsi[1]
+    LesHouches['RVKAPPAIN'].entries[3]=epsi[2]
+    LesHouches['RVSNVEVIN'].entries[1]=vi[0]
+    LesHouches['RVSNVEVIN'].entries[2]=vi[1]
+    LesHouches['RVSNVEVIN'].entries[3]=vi[2]
+##    To check with original SPheno.spc
+#    LesHouches['RVKAPPAIN'].entries[1]=veps[0]
+#    LesHouches['RVKAPPAIN'].entries[2]=veps[1]
+#    LesHouches['RVKAPPAIN'].entries[3]=veps[2]
+#    LesHouches['RVSNVEVIN'].entries[1]=vsnvev[0]
+#    LesHouches['RVSNVEVIN'].entries[2]=vsnvev[1]
+#    LesHouches['RVSNVEVIN'].entries[3]=vsnvev[2]
+## To write LesHouches.in in the right order.
+    LesHouches2={'AMODSEL':LesHouches['MODSEL'],'BSMINPUTS':LesHouches['SMINPUTS'],'CMINPAR':LesHouches['MINPAR'],'DEXTPAR':LesHouches['EXTPAR'],'ERVSNVEVIN':LesHouches['RVSNVEVIN'],'FRVKAPPAIN':LesHouches['RVKAPPAIN'],'GSPhenoInput':LesHouches['SPHENOINPUT']}
+    pyslha.writeSLHAFile('LesHouches.in',LesHouches2,decays)
     commands.getoutput('./bin/SPheno')
+##  Reading the new SPheno.spc
     nspc=pyslha.readSLHAFile('SPheno.spc')
     nsphenorp=nspc[0]['SPHENORP']
-    m2atm=nsphenorp.entries[7]
-    m2sol=nsphenorp.entries[8]
-    t2atm=nsphenorp.entries[9]
-    t2sol=nsphenorp.entries[10]
-    s2cho=nsphenorp.entries[11]
-    if (m2atm > 2.18e-3 and m2atm < 2.73e-3 and t2atm > 0.6393e0 and t2atm <1.7777e0 and m2sol > 7.09e-5 and m2sol < 8.19e-5 and t2sol > 0.3698e0 and t2sol < 0.5625e0) and s2cho > 0.0e0 and s2cho < 0.035e0:
+##  nuetrino oscilation parameters 
+    Delta2m32=nsphenorp.entries[7]
+    Delta2m21=nsphenorp.entries[8]
+    t223=nsphenorp.entries[9]
+    t212=nsphenorp.entries[10]
+    s213=nsphenorp.entries[11]
+# Checking neutrinosbounds at 3 sigma   
+    if (Delta2m32 > 2.18e-3 and Delta2m32 < 2.73e-3 and t223 > 0.6393e0 and t223 <1.7777e0 and Delta2m21 > 7.09e-5 and Delta2m21 < 8.19e-5 and t212 > 0.3698e0 and t212 < 0.5625e0) and s213 > 0.0e0 and s213 < 0.056e0:
         print "slt found at i=",i
-        print >> datos, m2atm, t2atm, m2sol,t2sol,s2cho, lambi[0], lambi[1], lambi[2], epsi[0], epsi[1], epsi[2]
+        print >> datos, Delta2m32, t223, Delta2m21,t212,s213, lambi[0], lambi[1], lambi[2], epsi[0], epsi[1], epsi[2]
 #
 datos.close()
 #3E-02 0.6 ! epsilon_1
